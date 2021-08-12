6ccad5dbd675091a4d5f8b1bb667882bf2f821e5
Jhonatan Veras <jveras@lerotmerlin.com.br>
2021-08-12 19:58:25 -0300

    styled-components

---
+++ my-app/src/styles/global.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+
+export default createGlobalStyle`
+  body{
+      background-color: yellow ;
+  }
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
