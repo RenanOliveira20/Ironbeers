70489226584c4f89fbffa65c5b2c8dcaf87dab14
Jhonatan Veras <jveras@lerotmerlin.com.br>
2021-08-16 18:28:00 -0300

    renderizaçao carrinho alteraçoes

--- my-app/src/components/ListCart/ListElements.js
+++ my-app/src/components/ListCart/ListElements.js
@@ -4,8 +4,8 @@ import { FaTrash } from "react-icons/fa";
 export const Div = styled.div`
   background-color: black;
   color: white;
-  margin: 30px;
-  width: 80%;
+  margin: 5px;
+  width: 85%;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -26,7 +26,7 @@ export const Li = styled.li`
   list-style: none;
   color: white;
   margin: 30px;
-  width: 100%;
+  width: 95%;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -59,7 +59,6 @@ export const Span = styled.span`
   @media screen and (max-width: 768px) {
     font-size: 15px;
     justify-content: flex-start;
-    
   }
 `;
 
@@ -67,26 +66,40 @@ export const Trash = styled(FaTrash)`
   color: black;
   width: 100px;
   height: 100px;
-  margin: 50px;
+  margin: 30px;
   cursor: pointer;
   @media screen and (max-width: 768px) {
-  color: black;
-  width: 50px;
-  height: 50px;
-  margin: 50px;
-  cursor: pointer;
+    color: black;
+    width: 50px;
+    height: 50px;
+    margin: 50px;
+    cursor: pointer;
+  }
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    color: red;
   }
 `;
 
 export const Input = styled.input`
-  width: 60px;
-  height: 60px;
+  width: 80px;
+  height: 50px;
   margin-inline-end: 2%;
   cursor: pointer;
+  font-size: 30px;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 5px;
   @media screen and (max-width: 768px) {
-  width: 25px;
+  width: 35px;
   height: 25px;
   margin-inline-end: 2%;
   cursor: pointer;
+  font-size: 15px;
+  }
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: yellow;
+    color: black;
   }
 `;
