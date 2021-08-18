f5272c1c520eec88efbf552d331e47eb007c6e4c
Renan Oliveira <reluiz@leroymerlin.com.br>
2021-08-17 21:39:01 -0300

    corrections-> passed to styled component single-beer page and linked to cart all buttons

---
+++ my-app/src/components/SingleCard/styles.js
@@ -0,0 +1,88 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Single = styled.div`
+   width :90%;
+   height: 600px;
+    background-color: black;
+    margin: 20px 5% 20px 5%;
+    border-radius: 15px;
+    display: flex;
+    justify-content: space-around;
+    padding: 20px 0 ;
+    div {
+        width: 50%;
+        display: flex;
+        justify-content: center;
+        img {
+            height:500px ;
+        }
+    }
+`;
+export const Rigth = styled.div `
+    background-color: white;
+    text-align: center;
+    margin-right: 2%;
+    border-radius: 15px;
+    color: black;
+    display: flex;
+    flex-direction: column;
+    justify-content : space-around;
+    h1 {
+        font-size: 50px
+    };
+    h2 {
+        font-size: 40px
+    };
+    h3 {
+        font-size: 30px
+    }
+`;
+export const Footer = styled.div `
+display: flex;
+justify-content: space-evenly;
+div {
+    background-color: black;
+    width: 25%;
+    color: white;
+    padding: 10px;
+    border-radius:15px ;
+    font-size:20px ;
+    display: flex;
+    justify-content: space-evenly;
+    flex-direction: column;
+    text-align: center;
+    span {
+    width:40%;
+    background-color: white;
+    color: black;
+    width: auto;
+    border-radius: 10px;
+};
+input{
+    background-color: white;
+    color: black;
+    width: auto;
+    border-radius: 10px;
+    text-align: center;
+};
+}
+
+`;
+export const LinkCart = styled(Link) `
+    font-size: 25px;
+    width: 30%;
+    padding: 10px;
+    background-color: #256ce1;
+    color: white;
+    border-top-left-radius:15px ;
+    border-end-end-radius: 15px;
+    text-decoration: none;
+    text-align: center;
+    align-items: center;
+    &:hover {
+    transition: all 0.2s ease-in-out;
+    background: black;
+    color: yellow;
+  }
+`
