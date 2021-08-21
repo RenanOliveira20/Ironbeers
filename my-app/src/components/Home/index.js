50aa5cf7b03249f7b154ca77ed5836e1fbc415b8
Renan Oliveira <reluiz@leroymerlin.com.br>
2021-08-21 10:53:41 -0300

    adapted

--- my-app/src/components/Home/index.js
+++ my-app/src/components/Home/index.js
@@ -6,29 +6,14 @@ import Footer from "../Footer/index";
 import "./style.css";
 class Home extends Component {
   state ={
-    beers : [],
-    vodkas: [],
-    whiskys: [],
-    gins: [],
-    liquors: [],
-    boxes: []
+    beers : []
   }
 
   async componentDidMount() {
     try {
       const getBeers = await api.getBeers('beers');
-      const getVodkas = await api.getBeers('vodkas');
-      const getWhiskys = await api.getBeers('whiskys');
-      const getGins = await api.getBeers('gins');
-      const getLiquors = await api.getBeers('liquors');
-      const getBoxes = await api.getBeers('boxes');
       this.setState({
-        beers: getBeers,
-        vodkas: getVodkas,
-        whiskys: getWhiskys,
-        gins: getGins,
-        liquors: getLiquors,
-        boxes: getBoxes
+        beers: getBeers
       });
     } catch (e) {
       this.setState({
@@ -51,36 +36,6 @@ class Home extends Component {
            return  <Card key={e.id} data={e} number={i} />;
           })}
           </Carousel>
-          <h1>Vodkas</h1>
-          <Carousel breakPoints={breakPoints }>
-          {this.state.vodkas.map((e,i) => {
-           return  <Card key={e.id} data={e} number={i} />;
-          })}
-          </Carousel>
-          <h1>Whiskys</h1>
-          <Carousel breakPoints={breakPoints }>
-          {this.state.whiskys.map((e,i) => {
-           return  <Card key={e.id} data={e} number={i} />;
-          })}
-          </Carousel>
-          <h1>Gins</h1>
-          <Carousel breakPoints={breakPoints }>
-          {this.state.gins.map((e,i) => {
-           return  <Card key={e.id} data={e} number={i} />;
-          })}
-          </Carousel>
-          <h1>Liquors</h1>
-          <Carousel breakPoints={breakPoints }>
-          {this.state.liquors.map((e,i) => {
-           return  <Card key={e.id} data={e} number={i} />;
-          })}
-          </Carousel>
-          <h1>Boxes</h1>
-          <div className = 'boxes'>
-          {this.state.boxes.map((e,i) => {
-           return  <Card key={e.id} data={e} number={i} />;
-          })}
-          </div>
         <Footer />
       </div>
     );
