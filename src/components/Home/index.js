7bed15ef1f725d73917aeddd9294186e309487f9
Renan Oliveira <reluiz@leroymerlin.com.br>
2021-08-24 17:24:39 -0300

    updates

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -57,7 +57,6 @@ class Home extends Component {
       {width : 200, itemsToShow: 2},
       {width : 500, itemsToShow: 3},
       {width : 900  , itemsToShow: 4},
-      {width : 1100  , itemsToShow: 5}
     ]
     return (
       <div>
